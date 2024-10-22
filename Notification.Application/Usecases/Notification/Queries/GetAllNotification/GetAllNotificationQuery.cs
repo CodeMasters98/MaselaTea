34c7a8e1eed34c6a1d924eb02eae2c7934421173
darvishiparham14@gmail.com <darvishiparham14@gmail.com>
2024-10-22 19:09:05 +0330

    Add new query

---
+++ Notification.Application/Usecases/Notification/Queries/GetAllNotification/GetAllNotificationQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Notification.Application.Wrappers;
+using Models = Notification.Domain.Entities;
+
+namespace Notification.Application.Usecases.Notification;
+
+public record GetAllNotificationQuery() : IRequest<Response<List<Models.Notification>>>;
---
+++ Notification.Application/Usecases/Notification/Queries/GetAllNotification/GetAllNotificationQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Notification.Application.Contracts;
+using Notification.Application.Wrappers;
+using Models = Notification.Domain.Entities;
+
+namespace Notification.Application.Usecases.Notification;
+
+public class GetAllNotificationQueryHandler(INotificationRepository notificationRepository) : IRequestHandler<GetAllNotificationQuery, Response<List<Models.Notification>>>
+{
+    public async Task<Response<List<Models.Notification>>> Handle(GetAllNotificationQuery request, CancellationToken cancellationToken)
+    {
+        List<Models.Notification> notifications = notificationRepository.GetAll();
+        return new Response<List<Models.Notification>>(notifications);
+    }
+}
