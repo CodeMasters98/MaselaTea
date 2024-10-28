c70da3ea73549a67eecfc3dc22bad7b2b21544c8
AmirHoseinAshkbar <amir.h.ashkbar81@gmail.com>
2024-10-28 14:42:08 +0330

    Changed int to bool

--- Notification.Application/Usecases/Notification/Commands/AddNotification/AddNotificationCommand.cs
+++ Notification.Application/Usecases/Notification/Commands/AddNotification/AddNotificationCommand.cs
@@ -4,4 +4,4 @@ using Notification.Domain.Enums;
 
 namespace Notification.Application.Usecases.Notification;
 
-public record AddNotificationCommand(string Message, string Reciever, NotficationType NotficationType) : IRequest<Response<int>>;
+public record AddNotificationCommand(string Message, string Reciever, NotficationType NotficationType) : IRequest<Response<bool>>;
--- Notification.Application/Usecases/Notification/Commands/AddNotification/AddNotificationCommandHandler.cs
+++ Notification.Application/Usecases/Notification/Commands/AddNotification/AddNotificationCommandHandler.cs
@@ -5,17 +5,18 @@ using Models = Notification.Domain.Entities;
 
 namespace Notification.Application.Usecases.Notification;
 
-public class AddNotificationCommandHandler(INotificationRepository notificationRepository) : IRequestHandler<AddNotificationCommand, Response<int>>
+public class AddNotificationCommandHandler(INotificationRepository notificationRepository) : IRequestHandler<AddNotificationCommand, Response<bool>>
 {
-    public async Task<Response<int>> Handle(AddNotificationCommand request, CancellationToken cancellationToken)
+    public async Task<Response<bool>> Handle(AddNotificationCommand request, CancellationToken cancellationToken)
     {
         var notification = new Models.Notification()
         {
             Message = request.Message,
             NotficationType = request.NotficationType,
             Reciever = request.Reciever,
+            
         };
-        notificationRepository.Add(notification);
-        return null;
+        var isAdded = notificationRepository.Add(notification);
+        return new Response<bool>(isAdded);
     }
 }
